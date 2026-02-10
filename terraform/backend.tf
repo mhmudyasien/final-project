7b84dd5c495471518baf189b4b9b141c7820e5ce
Mahmoud Yasien <mhmudlll4xm@gmail.com>
2026-02-11 00:08:40 +0200

    updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,9 @@
-# backend.tf
-
-# Example configuration for remote state management
-# PRE-REQUISITE: S3 Bucket and DynamoDB Table must exist
-/*
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "capstone/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "capstone-project-tf-state-1770757198"
+    key            = "final-project/terraform.tfstate"
+    region         = "us-east-2"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
 }
-*/
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,9 +8,9 @@ output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "alb_dns_name" {
-  value = module.alb.alb_dns_name
-}
+# output "alb_dns_name" {
+#   value = module.alb.alb_dns_name
+# }
 
 output "rds_endpoint" {
   value = module.rds.db_endpoint
